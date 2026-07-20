691f8782bf0ddea05663dae8c90cfb54c3ca4eac
mgjang8428 <mgjang8428@gmail.com>
2026-07-20 18:11:06 +0900

    chore: 프로젝트 생성
    * goal 프로젝트를 생성하였음
    * local 환경설정 파일을 생성하고 설정하였음

---
+++ goal/src/main/java/com/mik/goal/GoalApplication.java
@@ -0,0 +1,13 @@
+package com.mik.goal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GoalApplication.class, args);
+	}
+
+}
